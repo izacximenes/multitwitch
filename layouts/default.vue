bb5db8f3dd106d2927895a42eb890bc805405d7f
izacximenes <izac98ximenes@gmail.com>
2021-05-07 19:25:44 -0300

    adicionado bottom padding

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col h-screen w-screen overflow-x-hidden">
     <nav-bar></nav-bar>
     <modal-add-channel></modal-add-channel>
-    <div class="flex-grow">
+    <div class="flex-grow pb-16">
       <Nuxt />
     </div>
 
